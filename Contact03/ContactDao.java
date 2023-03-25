d167c703a14c8e2bcc91e826f58ff83332c31e17
drj9812 <drj9812@gmail.com>
2023-03-25 19:02:50 +0900

    Contact Program V0.3

---
+++ Contact03/ContactDao.java
@@ -0,0 +1,48 @@
+package edu.java.contact03;
+
+import edu.java.contact.model.Contact;
+
+//MVC(Model-View-Controller) : 아키텍쳐에서 Controller 역할을 수행할 인터페이스(구현할 클래스가 있어야함)
+//DAO(Data Access Object) : 데이터를 사용해서 비즈니스 로직(기능)을 수행하는 객체 (저장, 수정, 검색, 삭제 등등)
+//비슷한 성격을 가지고 있는 클래스들을 폴더로 구분해서 그 폴더들의 밑에다가 각각의 파일들을 저장하는 게 패키지
+
+
+public interface ContactDao {
+	
+	/**
+	 * 새 연락처 저장 기능. 연락처 객체를 배열에 저장.
+	 * @param c 배열에 저장할 연락처 객체.
+	 * @return 저장에 성공하면 1을 리턴, 실패하면 0을 리턴
+	 */
+	int create(Contact c); //인터페이스라 public abstract 생략 가능함
+	//int create(String name, String phone, String email)도 가능함(아규먼트를 한개 가질 것인가 여러개 가질 것인가의 차이)
+	/**
+	 * 연락처 전체 목록 보기 기능. 저장된 연락처 개수 크기의 연락처 배열을 리턴.
+	 * @return 연락처 배열(Contact[])을 리턴해줄 수 있으면 Main은 화면에다가 그거를 print하면 됨<- 이걸 생각하면서 설계
+	 */
+	Contact[] read();
+	
+	/**
+	 * 인덱스 검색. 전달받은 인덱스 위치에 있는 연락처 객체를 리턴.
+	 * @param index 검색할 인덱스. 0 이상의 정수.
+	 * @return 해당 인덱스의 연락처(Contact 타입) 객체이거나 또는 null(해당하는 인덱스가 null인 경우(아규먼트에 음수가 들어오거나 배열의 크기를 뛰어넘는 경우))
+	 * 
+	 */
+	Contact read(int index); // read() 인터페이스 오버로딩
+	
+	/**
+	 * 인덱스 수정 기능
+	 * @param index 수정할 연락처의 인덱스. 0 이상이고, 저장된 연락처 개수보다 작은 정수
+	 * @param contact(type) 수정할 연락처 정보의 객체.
+	 * @return 수정 성공하면 1을 리턴하고 실패하면 0을 리턴
+	 */
+	int update(int index, Contact contact);
+	
+	/**
+	 * 연락처 삭제 기능
+	 * @param index 삭제할 연락처의 인덱스. 0 이상이고, 저장된 연락처 개수보다 작은 정수.
+	 * @return 삭제 성공하면 1, 실패하면 0.
+	 */
+	int delete(int index);
+	
+}
---
+++ Contact03/ContactDaoImpl.java
@@ -0,0 +1,134 @@
+package edu.java.contact03;
+
+import edu.java.contact.model.Contact;
+
+//MVC(Model - View - Controller) 아키텍쳐에서 인터페이스로 설계한 컨트롤러(Dao)를 구현하는 클래스
+//singleton 디자인 패턴을 적용.
+//version2에선 메인에서 하던 기능들을 이젠 전부 이 클래스에서 해결할 거임
+
+public class ContactDaoImpl implements ContactDao {
+//ContactDaoImpl 객체를 여러개 생성하게 되면 각각의 객체들마다 연락처 배열을 가지게 됨(저장, 읽기가 다를 수 있으므로 singleton 패턴을 적용하는 것)
+
+	// singleton step 1
+
+	private static ContactDaoImpl instance = null;
+
+	// singleton step 2
+	// constructor
+	private ContactDaoImpl() {
+	} // 생성자를 private으로 설정하면 자신의 클래스에서만 생성됨. 외부에선 x
+
+	// singleton step 3
+	public static ContactDaoImpl getInstance() { // getInstance()를 몇번 호출하던 최초로 생성된 객체의 주소값만 저장됨
+		if (instance == null) {// 객체 생성 전이라면
+			instance = new ContactDaoImpl();
+		}
+		return instance;
+
+	}
+	// 위 3단계가 충족되면 singleton 패턴이 완성됨
+
+	// fields
+	private static final int MAX_LENGTH = 5; // 배열의 길이(크기), 다른 클래스에선 보여지지 않기 위해 private으로 선언
+	// -> 인터페이스 ContactDao에서 선언해도 되지만 그러면 public 으로 다른 클래스에서 보여지기 때문에 private으로 선언할
+	// 수 있는 class 에서 선언
+	private Contact[] contacts = new Contact[MAX_LENGTH];
+	private int count = 0; // 배열에 현재까지 저장된 연락처 객체의 개수.
+	// ->배열에 연락처를 저장할 때 +1, 배열에서 연락처를 삭제할 때 -1
+
+	// methods
+	/**
+	 * 연락처 배열에 새로운 연락처 객체를 저장할 수 있는 공간이 남아있는지를 리턴.
+	 * 
+	 * @return 배열에 빈 공간이 있으면 true, 그렇지 않으면 false.
+	 */
+	// 배열에 더 저장할 공간이 있는지 없는지 알려주는 메서드
+	public boolean isMemoryAvailable() {
+		return count < MAX_LENGTH;
+	}
+
+//	ContactDaoImpl 클래스의 멤버들이 private로 선언되어있기 때문에 배열의 크기가 몇인지 현재 인덱스 몇까지
+	// 저장되어있는지 외부에선 알 수 없기 때문에 public method를 생성해서 접근하는 것
+
+	/**
+	 * 어떤 index가 검색, 수정, 삭제할 때 사용 가능한 범위 안에 있는 인덱스인지를 리턴
+	 * 
+	 * @param index 유효한 인덱스인지 검사할 인덱스.
+	 * @return 사용가능한 인덱스는 true, 그렇지 않으면 false를 리턴
+	 */
+	public boolean isValidIndex(int index) {
+		return (index >= 0) && (index < count);
+	}
+
+	// CRUD(Create, Read, Update, Delete) 기능 메서드들 :
+	@Override
+	public int create(Contact c) {
+		// 이미 이 메서드에 왔다는 건 메모리에 공간이 남아있다는 뜻이지만 한번 더 검사
+		// 자기가 자기 메서드 부르는 건 언제든지 가능함
+		if (isMemoryAvailable()) { // 배열에 빈 공간이 있으면
+			contacts[count] = c;
+			count++;
+
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	@Override
+	public Contact[] read() {
+		Contact[] array = new Contact[count];
+		//->얘를 만든 목적은 contacts 배열에 저장된 원소 개수의 크기를 갖는 새로운 배열을 생성하려고
+		
+		for (int i = 0; i < count; i++) {
+			array[i] = contacts[i];
+		}
+		return array;
+	}
+
+	@Override
+	public Contact read(int index) {
+		if (isValidIndex(index)) {
+			return contacts[index];
+		} else {
+			return null;			
+		}
+	}
+
+	@Override
+	public int update(int index, Contact contact) {
+//		if (isValidIndex(index)) {
+////			contacts[index].setName(contact.getName());
+////			contacts[index].setPhone(contact.getPhone());
+////			contacts[index].setEmail(contact.getEmail());
+//			contacts[index] = contact; //위 세줄을 한 줄로
+//			return 1;
+//		} else {
+//			return 0;
+//		}
+		
+		if (!isValidIndex(index))
+			return 0;
+		contacts[index] = contact;
+		return 1;
+	}
+
+	@Override
+	public int delete(int index) {
+		if (!isValidIndex(index)) 
+			return 0;
+		
+		for (int i = index; i < count - 1; i++) {
+			contacts[i] = contacts[i + 1];
+					
+		}
+		contacts[count - 1] = null;
+		count--;
+		
+		return 1;
+//		contacts[index] = null;
+//		count--;
+//		return 1;
+	}
+
+}
